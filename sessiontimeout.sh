7c98d49f91f1d11193bca4a9966339b23d300b54
Arun Yadav <aruner02@gmail.com>
2020-10-02 14:39:21 +0530

    TotalSession timeout
    Calculates Session timeout using variables "ClientAliveInterval" & "ClientAliveCountMax"

---
+++ sessiontimeout.sh
@@ -0,0 +1,31 @@
+
+a=$(cat /etc/ssh/config.conf | grep ClientAliveInterval | grep ^[^#] | grep -o '[0-9]*$')
+b=$(cat /etc/ssh/config.conf | grep ClientAliveCountMax | grep ^[^#] | grep -o '[0-9]*')
+
+if [[ ${a: -1} -eq 'm' ]]
+then
+if [[ $b -eq 0 ]]
+then
+echo ${a%?}m                       
+else
+echo $((${a%?}*$b))m          
+fi
+elif [[ ${a: -1} -eq 's' ]] 
+then
+if [[ $b -eq 0 ]]
+then
+echo $((${a%?}/60))m
+else
+echo $(((${a%?}*$b)/60))m
+fi
+elif [[ ${a: -1}  =~ ^[0-9]+$ ]]
+then
+if [[ $b -eq 0 ]]
+then
+echo $(($a/60))m
+else
+echo $((($a*$b)/60))m
+fi
+else
+echo 'fail'
+fi
